60a0cef6ed6a1c31871ed40cb0b047aa260419a6
omghb <omghb@users.noreply.github.com>
2020-08-25 08:39:31 +0200

    Android sample solution

---
+++ XamarinBindingIssue2/XamarinBindingIssue2/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Waf.Foundation;
+
+namespace XamarinBindingIssue2
+{
+    public class MainViewModel : Model
+    {
+        private TimeSpan? time = TimeSpan.FromSeconds(42);
+
+        public TimeSpan? Time
+        {
+            get => time;
+            set => SetProperty(ref time, value);
+        }
+    }
+}
